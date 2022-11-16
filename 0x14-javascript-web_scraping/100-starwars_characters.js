59dcdffc22975cb401413066845ae15b42540978
ShaddyHamster <101396528+ShaddyHamster@users.noreply.github.com>
2022-11-16 11:19:53 +0300

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,21 +1,17 @@
 #!/usr/bin/node
 
-const req = require('request');
-const id = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/';
-req.get(url + id, function (error, res, body) {
+const request = require('request');
+
+request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
   if (error) {
-    console.log(error);
+    console.error(error);
   }
-  const data = JSON.parse(body);
-  const dd = data.characters;
-  for (const i of dd) {
-    req.get(i, function (error, res, body1) {
+  JSON.parse(body).characters.forEach(function (url, callback) {
+    request(url, function (error, response, body) {
       if (error) {
-        console.log(error);
+        console.error(error);
       }
-      const data1 = JSON.parse(body1);
-      console.log(data1.name);
+      console.log(JSON.parse(body).name);
     });
-  }
+  });
 });
